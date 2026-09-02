e47dc14130bc66ee82e7bddd9bc3ab7b0ed6fa9b
Darsha Anuradha <122999771+darshaanuradha@users.noreply.github.com>
2026-09-02 22:29:30 +0530

    Initial commit

---
+++ src/main/java/com/uovt/student_management_system_be/StudentManagementSystemBeApplication.java
@@ -0,0 +1,13 @@
+package com.uovt.student_management_system_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementSystemBeApplication.class, args);
+    }
+
+}
